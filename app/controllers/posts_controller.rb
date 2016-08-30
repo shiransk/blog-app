d59a919773d20e210727a2f66bd1fcf6d5d3a895
Shiran Koriat <shiransk@gmail.com>
2016-08-29 23:35:04 -0700

    add create and new actions to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
 
   def new 
+
   end
 
   def create
+    post = Post.new(name: params[:name], post_text: params[:post_text], user_id: params[:user_id])
+    if post.save
+      redirect_to '/'
+    else
+      redirect_to '/sessions/login'
+    end
   end
 
-  
+  def edit 
+
+  end
+
+  def update
+    
+  end
+
+
+
+
+
 end
